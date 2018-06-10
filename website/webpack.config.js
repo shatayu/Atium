13c04c641d72cf3109e79fc107c5d4c28e92fd3a
shatayu <shatayu@outlook.com>
2018-06-09 18:45:14 -0700

    keyword detection, summarizing working

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,5 +1,19 @@
+let path = require("path");
 
 module.exports = {
+    entry: {
+        compiledLoginPage: "./src/LoginPage.js",
+        compiledSettings: "./src/Settings.js",
+        compileProjectView: "./src/ProjectView.js",
+        compileMainPage: "./src/MainPage.js",
+        compileCitationPage: "./src/CitationPage.js",
+    },
+
+    output: {
+        path: path.join(__dirname, "./dist"),
+        filename: "[name].js"
+    },
+
     module: {
         rules: [
             {
